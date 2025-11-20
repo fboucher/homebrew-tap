950579f6c58d0cf74b5507eaf78ba9d730a988b5
Frank Boucher <2404846+fboucher@users.noreply.github.com>
2025-11-20 14:42:47 -0500

    Update SHA256 checksum for arm64 release

--- be-my-eyes.rb
+++ be-my-eyes.rb
@@ -21,7 +21,7 @@ class BeMyEyes < Formula
     end
     if Hardware::CPU.arm?
       url "https://github.com/fboucher/be-my-eyes/releases/download/v0.0.32/be-my-eyes_0.0.32_darwin_arm64.tar.gz"
-      sha256 "303f992a1365ff47e0438a2d452f7c8498338669090ae098174cea22aba3f387"
+      sha256 "0019dfc4b32d63c1392aa264aed2253c1e0c2fb09216f8e2cc269bbfb8bb49b5"
 
       def install
         bin.install "be-my-eyes"
