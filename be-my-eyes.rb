466b6d9fda974d8a76d9cf658d54c393f1ccd4ac
Frank <fboucher@outlook.com>
2025-12-05 07:40:56 -0500

    Add Linux support and fix formula URL issue

--- be-my-eyes.rb
+++ be-my-eyes.rb
@@ -4,11 +4,23 @@ class BeMyEyes < Formula
   version "0.1.14"
   license "MIT"
 
-  if OS.mac?
+  on_macos do
     if Hardware::CPU.arm?
       url "https://github.com/fboucher/be-my-eyes/releases/download/v0.1.14/be-my-eyes_0.1.14_darwin_arm64.tar.gz"
+      sha256 "PLACEHOLDER_ARM64"
     else
       url "https://github.com/fboucher/be-my-eyes/releases/download/v0.1.14/be-my-eyes_0.1.14_darwin_amd64.tar.gz"
+      sha256 "PLACEHOLDER_AMD64"
+    end
+  end
+
+  on_linux do
+    if Hardware::CPU.arm? && Hardware::CPU.is_64_bit?
+      url "https://github.com/fboucher/be-my-eyes/releases/download/v0.1.14/be-my-eyes_0.1.14_linux_arm64.tar.gz"
+      sha256 "PLACEHOLDER_LINUX_ARM64"
+    else
+      url "https://github.com/fboucher/be-my-eyes/releases/download/v0.1.14/be-my-eyes_0.1.14_linux_amd64.tar.gz"
+      sha256 "PLACEHOLDER_LINUX_AMD64"
     end
   end
 
